3fd7e815bd7f65d675623556da58c13325c17107
Ville Korhonen <ville@xd.fi>
2013-02-26 06:08:23 +0200

    Add favicon using hook. Fixes #13

--- functions.php
+++ functions.php
@@ -445,3 +445,8 @@ function vino_scripts() {
 	wp_enqueue_script('siteactions', plugins_url('/js/siteactions.js', __FILE__), array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'vino_scripts');
+
+function vino_favicon() { ?>
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png"/>
+<?php }
+add_action('wp_head', 'vino_favicon');
